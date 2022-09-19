172abf2bfb2a63e9ad7c20a0d08fcf8ec9c865c1
K4ynan <kaynancamilo12@gmail.com>
2022-09-19 18:55:12 -0300

    calculo

---
+++ script.js
@@ -0,0 +1,27 @@
+function soma() {
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    var conta = n1 + n2
+    document.getElementById('resultado').innerHTML = `<p>A soma de ${n1} e ${n2} é igual a ${conta}</p>`
+}
+
+function subtrair() {
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    var conta = n1 - n2
+    document.getElementById('resultado').innerHTML = `<p>A subtração de ${n1} e ${n2} é igual a ${conta}</p>`
+}
+
+function multiplicaçao() {
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    var conta = n1 * n2
+    document.getElementById('resultado').innerHTML = `<p>A multiplicação de ${n1} e ${n2} é igual a ${conta}</p>`
+}
+
+function divisao() {
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    var conta = n1 / n2
+    document.getElementById('resultado').innerHTML = `<p>A divisão de ${n1} e ${n2} é igual a ${conta}</p>`
+}
